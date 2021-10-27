e01a83712136e7e70c38c82bc9db2a3c70cdb0bf
andrew <andrew19@live.fr>
2021-10-27 19:53:05 +0200

    time ok

--- ft_client.c
+++ ft_client.c
@@ -6,39 +6,47 @@
 /*   By: andrew <andrew@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 10:39:58 by acolin            #+#    #+#             */
-/*   Updated: 2021/10/26 20:16:45 by andrew           ###   ########.fr       */
+/*   Updated: 2021/10/27 19:44:40 by andrew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
+void	ft_aff_nbbytes(int size)
+{
+	ft_putstr_fd("envoyer : ", 1);
+	ft_putnbr_fd(size, 1);
+	ft_putstr_fd(" bytes\n", 1);
+}
+
 void	ft_cmpt(int s)
 {
+	if (s == SIGUSR2)
+		exit();
 }
 
 void	int_dec(unsigned char c, int pid)
 {
-	int i;
+	int	i;
 
 	i = 8;
 	while (i)
 	{
-		if((c >> (i - 1)) & 1)
+		if ((c >> (i - 1)) & 1)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
 		i--;
-		pause();
-		usleep(200);
+		usleep(600);
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	int i;
+	int	i;
 	int	size;
-	int pid;
-	
+	int	pid;
+
 	signal(SIGUSR1, ft_cmpt);
 	signal(SIGUSR2, ft_cmpt);
 	if (argc > 1 && argc < 4)
@@ -56,9 +64,7 @@ int	main(int argc, char *argv[])
 			}
 			int_dec((unsigned char) '\0', pid);
 			size += 8;
-			ft_putstr_fd("envoyer : ", 1);
-			ft_putnbr_fd(size, 1);
-			ft_putstr_fd(" bytes\n", 1);
+			ft_aff_nbbytes(size);
 		}
 	}
 	return (1);
--- ft_server.c
+++ ft_server.c
@@ -6,13 +6,13 @@
 /*   By: andrew <andrew@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 10:40:14 by acolin            #+#    #+#             */
-/*   Updated: 2021/10/26 22:21:54 by andrew           ###   ########.fr       */
+/*   Updated: 2021/10/27 19:46:31 by andrew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-void	ft_aff_pid()
+void	ft_aff_pid(void)
 {
 	ft_putstr_fd("my pid : ", 1);
 	ft_putnbr_fd(getpid(), 1);
@@ -29,7 +29,7 @@ void	ft_aff_char(unsigned char *c, int *size)
 		*size = 0;
 	}
 	else
-       	ft_putchar_fd(*c, 1);
+		ft_putchar_fd(*c, 1);
 }
 
 void	on_signal(int s, siginfo_t *sigt, void *v)
@@ -41,33 +41,27 @@ void	on_signal(int s, siginfo_t *sigt, void *v)
 	c |= s == SIGUSR2;
 	i++;
 	size++;
-    if (i < 8)
-        c <<= 1;
-    if (i == 8)
+	if (i < 8)
+		c <<= 1;
+	if (i == 8)
 	{
 		ft_aff_char(&c, &size);
 		i = 0;
 		c = 0;
 	}
-	if (s == SIGUSR1)
-		kill(sigt->si_pid, SIGUSR2);
-	else
-		kill(sigt->si_pid, SIGUSR1);
+	kill(sigt->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
 	struct sigaction	ac;
-	
+
 	ft_aff_pid();
 	ac.sa_flags = SA_SIGINFO;
 	ac.sa_sigaction = on_signal;
 	sigaction(SIGUSR1, &ac, NULL);
 	sigaction(SIGUSR2, &ac, NULL);
-	while(1)
-	{
-   		pause();
-		usleep(200);
-	}
+	while (1)
+		pause();
 	return (1);
 }
